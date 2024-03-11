a7a686775b4474867d7314fd07c8ca173c473259
ignasbudreika <ign.budreika@gmail.com>
2024-03-11 21:13:35 +0200

    Add text view and display button

--- app/src/main/java/com/example/helloignas/MainActivity.kt
+++ app/src/main/java/com/example/helloignas/MainActivity.kt
@@ -2,10 +2,19 @@ package com.example.helloignas
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        var showButton = findViewById<Button>(R.id.displayButton)
+        var textView = findViewById<TextView>(R.id.textView)
+
+        showButton.setOnClickListener {
+            textView.text = "Hello from Ignas Budreika"
+        }
     }
 }
